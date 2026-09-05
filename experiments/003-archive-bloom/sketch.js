b683e1e48e01e333a63301ec210a818d542dfe4e
eVa <ebruvatanseven@gmail.com>
2026-09-05 21:37:35 +0200

    Create sketch.js
    Create Archive Bloom drawing behaviour

---
+++ experiments/003-archive-bloom/sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(400, 400);
+  background(20);
+  angleMode(DEGREES);
+}
+
+function mousePressed() {
+  drawFlower(mouseX, mouseY);
+}
+
+function drawFlower(x, y) {
+  let petals = floor(random(5, 17));
+  let size = random(20, 50);
+
+  push();
+  translate(x, y);
+  rotate(random(360));
+
+  noStroke();
+  fill(150, 175, 125, 80);
+
+  for (let i = 0; i < petals; i++) {
+    ellipse(0, size / 3, size / 3, size);
+    rotate(360 / petals);
+  }
+
+  fill(210, 185, 100);
+  circle(0, 0, size / 4);
+
+  pop();
+}
