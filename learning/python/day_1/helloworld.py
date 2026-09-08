815bb378ae47200f91327a3a7cb1b67afdf51d30
eVa <ebruvatanseven@gmail.com>
2026-09-08 16:54:10 +0200

    Preserve Python Day 1 exercises with fictional profile details

---
+++ learning/python/day_1/helloworld.py
@@ -0,0 +1,61 @@
+# Personal details below are fictional examples.
+#Day 1 30DaysOfPython Challenge
+print(2 + 3)  # addition(+)
+print(3 - 1)  # subtraction(-)
+print(2 * 3)  # multiplication(*)
+print(3 / 2)    # division(/)
+print(3 ** 2)   # exponential (**)
+print(3 % 2)    # modulus(%)
+print(3 // 2)   # Floor division operator(//)
+
+# Checking data types
+print(type(10)) # Int
+print(type(3.14)) # Float
+print(type(1 + 3j)) # Complex number
+print(type('Mira'))  # String
+print(type([1, 2, 3]))    # List
+print(type({'name' : 'Mira'}))   # Dictionary
+print(type({9.8, 3.14, 2.7}))   # Set
+print(type((9.8, 3.14, 2.7)))   # Tuple
+
+# Day 1 exercises
+
+import sys
+print(sys.version)
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(3 // 4)
+print(3 % 4)
+print(3 ** 4)
+
+print("Mira")
+
+print(type(10))
+
+print("Fernwick")
+print("Elderglen")
+print("I am enjoying 30 days of python")
+
+
+
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Mira', 'Python', 'Elderglen']))
+print(type("Mira"))
+print(type("Fernwick"))
+print(type("Elderglen"))
+
+print(type(True))
+print(3 < 4)
+print(3 > 4)
+
+print(type("True"))
+
+
+# Distance between (2, 3) and (10, 8)
+distance = ((10 - 2) ** 2 + (8 - 3) ** 2) ** 0.5
+print(distance)
